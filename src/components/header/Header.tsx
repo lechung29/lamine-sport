1ac1b393cfd859f197b3e195185ee76a0942cc68
Killian.Le <killian.le@avepoint.com>
2025-05-16 00:52:24 +0700

    Update header search box

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,6 +11,8 @@ import { Link } from "react-router-dom";
 import { Badge, Popover } from "antd";
 import { AccountMenu } from "../account-menu";
 import { CartMenu } from "../cart-menu";
+import { GoClock } from "react-icons/go";
+import { IoIosCloseCircleOutline, IoIosTrendingUp } from "react-icons/io";
 
 const Header: React.FC = () => {
     return (
@@ -22,19 +24,54 @@ const Header: React.FC = () => {
                 </div>
                 <div className="relative group max-[950px]:hidden flex flex-1 h-10 overflow-visible">
                     <input type="text" placeholder="Tìm sản phẩm..." className="flex-1 !px-3 text-sm !text-[#333] outline-none border-none !bg-white" />
-                    <div className="relative w-10 overflow-hidden">
+                    <div className="relative group/search-button w-10 overflow-hidden">
                         <div
-                            className="absolute inset-0 bg-gray-200 group-hover:bg-[#77e322] transition-all duration-300 
+                            className="absolute inset-0 bg-gray-200 group-hover/search-button:bg-[#77e322] transition-all duration-300 
                             before:absolute before:top-0 before:left-0 before:h-full before:w-full 
-                            before:bg-[#77e322] before:transform before:scale-x-0 group-hover:before:scale-x-100 
+                            before:bg-[#77e322] before:transform before:scale-x-0 group-hover/search-button:before:scale-x-100 
                             before:origin-left before:transition-transform before:duration-300"
                         />
                         <div className="relative z-10 h-full flex items-center justify-center text-xl cursor-pointer">
-                            <IoMdSearch className="text-gray-800 group-hover:text-white" />
+                            <IoMdSearch className="text-[#333] group-hover/search-button:text-white" />
                         </div>
                     </div>
 
-                    <div className="absolute top-full left-0 w-full h-[50px] bg-white shadow-primary z-50 hidden group-focus-within:block">Hello</div>
+                    <div className="absolute top-full left-0 w-full min-h-[50px] !px-3 !pb-3 bg-white shadow-primary z-50 block group-focus-within:block">
+                        <div className="w-full !py-2 !text-[#333] flex items-center justify-start gap-1 !border-b !border-gray-200">
+                            <GoClock className="text-xl" />
+                            <span className="text-sm uppercase font-semibold">Tìm kiếm gần đây</span>
+                        </div>
+                        <div className="w-full !pt-4 !pb-2 !pr-10 flex items-center justify-start flex-wrap gap-2">
+                            <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                                <span className="text-sm text-[#333] group-hover/search-item:!text-white">Giày chạy</span>
+                                <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                            </div>
+                            <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                                <span className="text-sm text-[#333] group-hover/search-item:!text-white">Áo chống nắng</span>
+                                <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                            </div>
+                            <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                                <span className="text-sm text-[#333] group-hover/search-item:!text-white">Quần áo bóng đá câu lạc bộ Barcelona</span>
+                                <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                            </div>
+                            <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                                <span className="text-sm text-[#333] group-hover/search-item:!text-white">Quần áo bóng đá câu lạc bộ Barcelona</span>
+                                <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                            </div>
+                        </div>
+                        <div className="w-full !py-2 !text-[#333] flex items-center justify-start gap-1 !border-b !border-gray-200">
+                            <IoIosTrendingUp className="text-xl" />
+                            <span className="text-sm uppercase font-semibold">Đề xuất phổ biến</span>
+                        </div>
+                        <div className="w-full !pt-4 !pb-2 !pr-10 flex items-center justify-start flex-wrap gap-2">
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Áo chạy bộ</div>
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Áo chống nắng</div>
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Tất/vớ</div>
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Nón leo núi</div>
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Bóng rổ</div>
+                            <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Giày bóng đá</div>
+                        </div>
+                    </div>
                 </div>
                 <div className="flex items-center justify-between gap-4">
                     <Link className="max-[1200px]:hidden flex items-center justify-center gap-1.5" to="tel:19001900">
@@ -51,7 +88,7 @@ const Header: React.FC = () => {
                             <span className="text-sm leading-4 !font-medium">7 cửa hàng</span>
                         </div>
                     </Link>
-                    <Popover placement="bottomRight" content={<AccountMenu />} getPopupContainer={() => document.body} overlayStyle={{ zIndex: 9999}} trigger="hover">
+                    <Popover placement="bottomRight" content={<AccountMenu />} getPopupContainer={() => document.body} overlayStyle={{ zIndex: 9999 }} trigger="hover">
                         <div className="flex items-center justify-center gap-1.5 cursor-pointer">
                             <LuUser className="text-3xl text-white" />
                             <div className="flex flex-col items-start">
@@ -62,7 +99,7 @@ const Header: React.FC = () => {
                             </div>
                         </div>
                     </Popover>
-                    <Popover placement="bottomRight" content={<CartMenu />} getPopupContainer={() => document.body} overlayStyle={{ zIndex: 9999}} trigger="hover">
+                    <Popover placement="bottomRight" content={<CartMenu />} getPopupContainer={() => document.body} overlayStyle={{ zIndex: 9999 }} trigger="hover">
                         <div className="flex items-center justify-center gap-1.5 cursor-pointer">
                             <Badge count={4} color="#77e322" style={{ color: "black", boxShadow: "none" }}>
                                 <MdOutlineShoppingCart className="text-3xl text-white" />
@@ -74,19 +111,54 @@ const Header: React.FC = () => {
             </div>
             <div className="relative group max-[950px]:flex hidden h-10 !mt-2 !mb-1 overflow-visible">
                 <input type="text" placeholder="Tìm sản phẩm..." className="flex-1 !px-3 text-sm !text-[#333] outline-none border-none !bg-white" />
-                <div className="relative w-10 overflow-hidden">
+                <div className="relative group/search-button w-10 overflow-hidden">
                     <div
-                        className="absolute inset-0 bg-gray-200 group-hover:bg-[#77e322] transition-all duration-300 
+                        className="absolute inset-0 bg-gray-200 group-hover/search-button:bg-[#77e322] transition-all duration-300 
                             before:absolute before:top-0 before:left-0 before:h-full before:w-full 
-                            before:bg-[#77e322] before:transform before:scale-x-0 group-hover:before:scale-x-100 
+                            before:bg-[#77e322] before:transform before:scale-x-0 group-hover/search-button:before:scale-x-100 
                             before:origin-left before:transition-transform before:duration-300"
                     />
                     <div className="relative z-10 h-full flex items-center justify-center text-xl cursor-pointer">
-                        <IoMdSearch className="text-gray-800 group-hover:text-white" />
+                        <IoMdSearch className="text-[#333] group-hover/search-button:text-white" />
                     </div>
                 </div>
 
-                <div className="absolute top-full left-0 w-full h-[50px] bg-white shadow-primary z-50 hidden group-focus-within:block">Hello</div>
+                <div className="absolute top-full left-0 w-full min-h-[50px] !px-3 !pb-3 bg-white shadow-primary z-50 block group-focus-within:block">
+                    <div className="w-full !py-2 !text-[#333] flex items-center justify-start gap-1 !border-b !border-gray-200">
+                        <GoClock className="text-xl" />
+                        <span className="text-sm uppercase font-semibold">Tìm kiếm gần đây</span>
+                    </div>
+                    <div className="w-full !pt-4 !pb-2 !pr-10 flex items-center justify-start flex-wrap gap-2">
+                        <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                            <span className="text-sm text-[#333] group-hover/search-item:!text-white">Giày chạy</span>
+                            <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                        </div>
+                        <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                            <span className="text-sm text-[#333] group-hover/search-item:!text-white">Áo chống nắng</span>
+                            <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                        </div>
+                        <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                            <span className="text-sm text-[#333] group-hover/search-item:!text-white">Quần áo bóng đá câu lạc bộ Barcelona</span>
+                            <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                        </div>
+                        <div className="!pr-1 !pl-2 !py-0.5 bg-gray-200 hover:!bg-[#1c2635] text-[#333] group/search-item flex items-center justify-between gap-1 cursor-pointer">
+                            <span className="text-sm text-[#333] group-hover/search-item:!text-white">Quần áo bóng đá câu lạc bộ Barcelona</span>
+                            <IoIosCloseCircleOutline className="text-lg text-[#333] group-hover/search-item:!text-white hover:!text-red-500 cursor-pointer" />
+                        </div>
+                    </div>
+                    <div className="w-full !py-2 !text-[#333] flex items-center justify-start gap-1 !border-b !border-gray-200">
+                        <IoIosTrendingUp className="text-xl" />
+                        <span className="text-sm uppercase font-semibold">Đề xuất phổ biến</span>
+                    </div>
+                    <div className="w-full !pt-4 !pb-2 !pr-10 flex items-center justify-start flex-wrap gap-2">
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Áo chạy bộ</div>
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Áo chống nắng</div>
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Tất/vớ</div>
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Nón leo núi</div>
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Bóng rổ</div>
+                        <div className="!px-2 !py-0.5 text-sm bg-gray-200 hover:!bg-[#1c2635] text-[#333] hover:!text-white text-center cursor-pointer">Giày bóng đá</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
