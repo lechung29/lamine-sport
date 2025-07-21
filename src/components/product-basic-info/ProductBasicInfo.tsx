b0ae8abe06a62402aa5c2b5f14d248c3457f2b85
Killian.Le <killian.le@avepoint.com>
2025-07-22 01:02:08 +0700

    Update product detail page

--- src/components/product-basic-info/ProductBasicInfo.tsx
+++ src/components/product-basic-info/ProductBasicInfo.tsx
@@ -6,12 +6,13 @@ import { TooltipLabel } from "../tooltip-label";
 import { classNames } from "@/utils";
 import { TfiRulerAlt } from "react-icons/tfi";
 import { InstructionSizeDialog } from "../instruction-size-dialog";
+import { LuMinus, LuPlus } from "react-icons/lu";
 
 export const productSize = ["S", "M", "L"];
 
 const ProductBasicInfo: React.FunctionComponent = () => {
     const [currentSize, setCurrentSize] = React.useState<string>("S");
-    const [isOpenInstructionSize, setIsOpenInstructionSize] = React.useState<boolean>(false)
+    const [isOpenInstructionSize, setIsOpenInstructionSize] = React.useState<boolean>(false);
     return (
         <React.Fragment>
             <div className="w-full text-primary text-sm">
@@ -38,8 +39,10 @@ const ProductBasicInfo: React.FunctionComponent = () => {
                             thích kiểu dáng dài che phần thắt lưng, cổ áo cao mềm mại và cổ tay áo phủ tay tăng độ ấm.
                         </p>
                     </div>
-                    <div className="!mb-5 bg-[#f5f5f5] !px-2.5 !py-1 !pr-15">
-                        <p className="inline !text-[#c30000] !font-bold text-3xl">175.000₫</p>
+                    <div className="relative flex items-center !mb-5 bg-[#f5f5f5] !px-2.5 !py-2 !pr-15 ">
+                        <p className="!text-[#c30000] !mr-2 !font-bold text-3xl">160.000₫</p>
+                        <p className="!text-[18px] line-through">200.000đ</p>
+                        <div className="absolute top-0 bottom-0 right-0 w-14 inline-flex items-center justify-center !bg-[#c30000] text-white font-semibold [clip-path:polygon(20%_0%,100%_0%,100%_100%,0%_100%)]">- 20%</div>
                     </div>
                     <div className="!mb-2">
                         <p className="text-[16px]">
@@ -66,14 +69,29 @@ const ProductBasicInfo: React.FunctionComponent = () => {
                             <span className="text-[16px] cursor-pointer hover:text-[#a2ff00]">Hướng dẫn chọn kích cỡ</span>
                         </TooltipLabel>
                     </div>
+                    <div className="inline-flex items-center gap-2 !mb-4">
+                        <span className="text-[16px] font-semibold">Số lượng:</span>
+                        <div className="flex items-center">
+                            <div className="w-7.5 h-10 flex items-center justify-center !border !border-[#ddd] cursor-pointer bg-[#f8f8f8] hover:bg-[#002d3a] group">
+                                <LuMinus className="!text-[10px] group-hover:text-white" />
+                            </div>
+                            <input type="text" className="h-10 w-12.5 text-center outline-none !border-y !border-gray-200" />
+                            <div className="w-7.5 h-10 flex items-center justify-center !border !border-[#ddd] cursor-pointer bg-[#f8f8f8] hover:bg-[#002d3a] group">
+                                <LuPlus className="!text-[10px] group-hover:text-white" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-1 !mb-4">
+                        <button className="flex-1 h-10.5 uppercase !text-[16px] !bg-[#002d3a] !text-white hover:!text-[#333] !px-6 !py-2.5 !font-semibold transition-colors duration-400 hover:!bg-[#77e322] [clip-path:polygon(0%_0%,100%_0%,95%_100%,0%_100%)] cursor-pointer">
+                            Thêm vào giỏ
+                        </button>
+                        <button className="flex-1 h-10.5 uppercase !text-[16px] !bg-[#a2ff00] !px-6 !py-2.5 !font-semibold transition-colors duration-400 hover:!bg-[#77e322] [clip-path:polygon(5%_0%,100%_0%,100%_100%,0%_100%)] cursor-pointer">
+                            Mua ngay
+                        </button>
+                    </div>
                 </div>
             </div>
-            {
-                isOpenInstructionSize && <InstructionSizeDialog
-                    isOpen={isOpenInstructionSize}
-                    onClose={() => setIsOpenInstructionSize(false)}
-                />
-            }
+            {isOpenInstructionSize && <InstructionSizeDialog isOpen={isOpenInstructionSize} onClose={() => setIsOpenInstructionSize(false)} />}
         </React.Fragment>
     );
 };
--- src/pages/product-details/ProductDetails.tsx
+++ src/pages/product-details/ProductDetails.tsx
@@ -10,15 +10,15 @@ const ProductDetails: React.FunctionComponent = () => {
             <Breadcrumbs />
             <div className="w-full h-auto !px-[45px]">
                 <div className="flex flex-wrap">
-                    <div className="w-full !px-2 md:w-1/2 lg:w-3/8">
+                    <div className="w-full !px-3 md:w-1/2 lg:w-3/8">
                         <ProductPhotoView photoList={productColor.concat(productColor).map(item => item.imgUrl!)} />
                     </div>
 
-                    <div className="w-full !px-2 md:w-1/2 lg:w-3/8">
+                    <div className="w-full !px-3 md:w-1/2 lg:w-3/8">
                         <ProductBasicInfo />
                     </div>
 
-                    <div className="w-full !px-2 lg:w-1/4">
+                    <div className="w-full !px-3 lg:w-1/4">
                         <div className="bg-red-100 p-4 h-full">Hỗ trợ & Khuyến mãi</div>
                     </div>
                 </div>
