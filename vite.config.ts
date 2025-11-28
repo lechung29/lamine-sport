70a557d77228f7f497b43044b13a0c7eab527da1
Killian Le <86923962+lechung29@users.noreply.github.com>
2025-11-28 11:01:55 +0700

    Add vite-plugin-static-copy to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
